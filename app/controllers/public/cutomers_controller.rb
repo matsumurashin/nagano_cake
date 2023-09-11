7cde158a5db2cdc748efee6b1d0e45fd08a192c5
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-09-11 14:12:59 +0000

    update:public/customers/show.html.erb

--- app/controllers/public/cutomers_controller.rb
+++ app/controllers/public/cutomers_controller.rb
@@ -1,5 +1,7 @@
 class Public::CutomersController < ApplicationController
   def show
+    @customer_new = Customer.new
+    @customer = Customer.find(params[:id])
   end
 
   def edit
@@ -7,4 +9,9 @@ class Public::CutomersController < ApplicationController
 
   def check
   end
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
+  end
+
 end
