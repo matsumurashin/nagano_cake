dfd108b1a0aae5a8eddd99183b68a039771b5f7a
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-09-19 06:55:31 +0000

    update:public/customers/show.html.erb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,17 +9,12 @@ class ApplicationController < ActionController::Base
   #   homes_top_path
   # end
 
-  def set_search
-    @search = Item.ransack(params[:q])
-    @search_products = @search.result
-  end
+
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
-  before_action :set_search
 
 end
