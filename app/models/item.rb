ece8e0512835eb20327ed46a784566d5d8e72cb4
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-09-21 14:06:58 +0000

    update:admin/items/new.html.erb

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,4 @@
 class Item < ApplicationRecord
-  def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "id", "introduction", "name", "price", "updated_at"]
-  end
 
   has_one_attached :image
 end
