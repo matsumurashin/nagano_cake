7ba894e974cdd055922388eb985ea6890e03be6f
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-09-27 14:06:05 +0000

    update:admin/customers/index,show.html.erb

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,10 +1,18 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customer = Customer.page(params[:page])
   end
 
   def show
+    @customer = Customer.find(params[:id])
   end
 
   def edit
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,4 +6,10 @@ class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  private
+
+  def item_params
+     params.require(:item).permit(:name, :introduction, :price, :image)
+  end
 end
