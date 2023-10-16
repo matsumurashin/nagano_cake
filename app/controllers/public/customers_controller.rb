2af05d0759ba307390bbcc3db5a3bf14abc29598
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-10-16 14:28:39 +0000

    update:public/customers/check.html.erb;;public/customers/edit.html.erb;;public/homes/top.html.erb

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -7,11 +7,26 @@ class Public::CustomersController < ApplicationController
     @customer = Customer.find(current_customer.id)
   end
 
+  def update
+    @customer = Customer.find(current_customer.id)
+    @customer.update(customer_params)
+    redirect_to customers_mypage_path
+  end
+
   def check
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def withdraw
+    @customer = Customer.find(current_customer.id)
+    @customer.update(is_deleted: true)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
   end
 
   def customer_params
-    params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
+    params.require(:customer).permit(:email, :first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number, :is_deleted)
   end
 
 end
