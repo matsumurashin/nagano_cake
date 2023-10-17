beac14701dc1093a855b407bd3cf2d9378d13c3d
matsumurashin <134196388+matsumurashin@users.noreply.github.com>
2023-10-17 15:08:15 +0000

    update:some html.erb

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -10,7 +10,7 @@ class Admin::ItemsController < ApplicationController
   end
 
   def index
-    @item = Item.page(params[:page])
+    @item = Item.all.page(params[:page]).per(10)
   end
 
   def show
